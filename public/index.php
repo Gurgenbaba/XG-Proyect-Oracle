345e920f66d9701e0608bc5d7d81b13bb4db4e52
Gurgen Baba <gurgenbaba.msi@outlook.de>
2026-03-01 00:41:22 +0100

    Implement install lock to prevent re-installs
    Added an install lock to prevent boot-up without installation.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
 
 use App\Core\Common;
+use App\Libraries\Functions;
 
 define('IN_LOGIN', true);
 define('XGP_ROOT', realpath(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
+
+// --- HARD INSTALL LOCK (prevents bootUp redirecting to /install) ---
+$installLock = XGP_ROOT . 'storage' . DIRECTORY_SEPARATOR . 'install.lock';
+if (!file_exists($installLock)) {
+    // If you want to force-disable install always:
+    // create the lock automatically OR show a message.
+    // I'd rather hard-fail to avoid accidental re-installs:
+    http_response_code(500);
+    exit('Missing install.lock (installer is disabled).');
+}
 
 require XGP_ROOT . 'app' . DIRECTORY_SEPARATOR . 'Core' . DIRECTORY_SEPARATOR . 'Common.php';
 
@@ -11,7 +22,6 @@ $system = new Common();
 $system->bootUp('home');
 
 $page = filter_input(INPUT_GET, 'page');
-
 if (is_null($page)) {
     $page = 'home';
 }
@@ -22,6 +32,5 @@ if (file_exists($file_name)) {
     include $file_name;
 
     $class_name = 'App\Http\Controllers\Home\\' . ucfirst($page) . 'Controller';
-
     (new $class_name())->index();
 }
